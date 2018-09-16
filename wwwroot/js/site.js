810d2293b6fd64b1c0cfaa2b60a52d6508133019
Rich Hildred <rhildred@gmail.com>
2018-09-16 17:21:18 -0400

    improved UX, made it more razor webish

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,28 @@
 ﻿// Write your Javascript code.
+$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+var sFrom = "you@" + Math.ceil(Math.random() * 1000);
+$("#uName").html(sFrom);
+function newMessage() {
+	message = $(".message-input input").val();
+	if($.trim(message) == '') {
+		return false;
+	}
+	$('<li class="sent"><img src="images/Y.svg" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
+    $.post("/", "From=" + encodeURIComponent(sFrom) +
+                        "&Body=" + encodeURIComponent(message), (data)=>{
+            console.log(data);
+            $('<li class="replies"><img src="images/B.svg" alt="" /><p>' + $(data).find("Message").html() + '</p></li>').appendTo($('.messages ul'));
+    });
+	$('.message-input input').val(null);
+	$('.contact.active .preview').html('<span>You: </span>' + message);
+	$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+};
+$('.submit').click(function() {
+  newMessage();
+});
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    newMessage();
+    return false;
+  }
+});
